3062808636411b5c2f186085651613a19965bb68
Ibragim <ibragim.ast@gmail.com>
2026-09-01 19:36:18 +0300

    Добавлена жирность заголовка в разделе Афиша

--- src/components/blocks/Afisha.tsx
+++ src/components/blocks/Afisha.tsx
@@ -39,23 +39,25 @@ export default function Afisha() {
   return (
     <section className="py-20 bg-brand-light">
       <div className="max-w-6xl mx-auto px-4 md:px-6 w-full">
-        <h2 className="text-3xl md:text-5xl text-brand-dark">Афиша</h2>
-      </div>
+        <h2 className="text-3xl md:text-5xl text-brand-dark font-bold font-sans">
+          Афиша
+        </h2>
 
-      {eventsData
-        .sort(
-          (a, b) =>
-            new Date(a.dateIso).getTime() - new Date(b.dateIso).getTime(),
-        )
-        .map((item) => (
-          <EventCard
-            key={item.id}
-            dayOfTheMonth={item.dayOfTheMonth}
-            month={item.month}
-            address={item.address}
-            description={item.description}
-          />
-        ))}
+        {eventsData
+          .sort(
+            (a, b) =>
+              new Date(a.dateIso).getTime() - new Date(b.dateIso).getTime(),
+          )
+          .map((item) => (
+            <EventCard
+              key={item.id}
+              dayOfTheMonth={item.dayOfTheMonth}
+              month={item.month}
+              address={item.address}
+              description={item.description}
+            />
+          ))}
+      </div>
     </section>
   );
 }
