29258c46fca57ed0e100798318a33e8051571052
Ibragim <ibragim.ast@gmail.com>
2026-09-02 13:14:14 +0300

    Рефакторинг подвала. Исправление адаптивной версии

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-brand-dark text-brand-light border-t border-white/10">
-      <div className="max-w-6xl mx-auto px-6 py-16 w-full grid grid-cols-1 lg:grid-cols-3 gap-12 items-start">
-        <div className="flex flex-col h-full justify-between gap-6 md:gap-12">
-          <h4 className="font-sans font-bold text-xl">
+      <div className="max-w-6xl mx-auto px-6 py-16 w-full grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-12 items-start">
+        <div className="flex flex-col gap-4 md:h-full md:justify-between">
+          <h4 className="font-sans font-bold text-lg md:text-xl">
             Государственный академический ансамбль танца «Вайнах»
           </h4>
           <p className="text-sm text-brand-light/60 mt-auto">
@@ -15,7 +15,7 @@ export default function Footer() {
 
         <nav
           aria-label="Дополнительная навигация"
-          className="flex flex-col gap-1"
+          className="flex flex-col gap-2 md:gap-4"
         >
           <Link
             className="transition-colors hover:text-brand-gold"
@@ -37,11 +37,11 @@ export default function Footer() {
           </Link>
         </nav>
 
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col gap-4 md:h-full md:justify-between">
           <p>г. Грозный, ул. Шейха Али Митаева, 6</p>
           <a href="tel:8(8712)22-58-12">8(8712)22-58-12</a>
           <p>gat_vainah@mail.ru</p>
-          <div className="flex items-center gap-6 mt-auto">
+          <div className="flex items-center gap-6 mt-4 md:mt-auto">
             <a
               href="https://vk.ru/ans_vaynah"
               target="_blank"
