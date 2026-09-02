72a37456cb3b2be5a2da429d95803bc633009554
Ibragim <ibragim.ast@gmail.com>
2026-09-02 11:28:39 +0300

    Добавлен раздел О нас

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main className="pt-32 pb-20 bg-brand-light">
+      <div className="max-w-6xl mx-auto px-4 md:px-6 w-full">
+        <h1 className="font-sans font-bold text-4xl md:text-5xl text-brand-dark">
+          О нас
+        </h1>
+      </div>
+    </main>
+  );
+}
