8c5c790cf7c00522ed77f8f0460d53dcb6cf4151
Ibragim <ibragim.ast@gmail.com>
2026-09-02 11:55:03 +0300

    Исправление адаптивности шапки

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,8 +2,8 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useA11y } from "@/context/A11yContext";
 import { useState } from "react";
+import { useA11y } from "@/context/A11yContext";
 import { eventsData } from "@/data/events";
 import { newsData } from "@/data/news";
 import { documentsData } from "@/data/documents";
